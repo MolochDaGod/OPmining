708db8e9fd7b4e5e8d8783243cc2047710e025df
MolochDaDev <90799999+MolochDaGod@users.noreply.github.com>
2024-12-17 16:09:29 -0600

    Update Opmine.bat

--- OPmine/Opmine/Opmine.bat
+++ OPmine/Opmine/Opmine.bat
@@ -1,4 +1,4 @@
 @cd /d "%~dp0"
-JZmine -a kawpow -o us-rvn.2miners.com:6060 -u RX86fcoJgs6h2HC2HZb8Qsf7QeNfQpzfmJ.worker
+JZmine -a kawpow -o us-rvn.2miners.com:6060 -u ADDRESSOFRVNGIVEN.worker
 
 pause
